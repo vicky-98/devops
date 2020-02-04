c6cfb5985169a214cb4f196e1871c0db34cedc11
K <vignesh.g.k@capgemini.com>
2020-02-04 14:44:12 +0530

    first message

---
+++ server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+  /**
+   * @param name of the person
+   * @return Hello message
+   */
+  public final String greet(final String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
